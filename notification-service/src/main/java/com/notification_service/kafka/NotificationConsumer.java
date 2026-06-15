0b52c433e6b678b267ef3631611361aa5ebe848b
Shaurya Shaan <shauryashaan01@gmail.com>
2026-06-15 16:01:46 +0530

    email sending with stater-email dependency

--- notification-service/src/main/java/com/notification_service/kafka/NotificationConsumer.java
+++ notification-service/src/main/java/com/notification_service/kafka/NotificationConsumer.java
@@ -2,18 +2,28 @@ package com.notification_service.kafka;
 
 import com.notification_service.constants.AppConstants;
 import com.notification_service.dto.OrderEvent;
+import com.notification_service.service.EmailService;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotificationConsumer {
 
+    private EmailService emailService;
+
+    public NotificationConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
     @KafkaListener(
             topics = AppConstants.TOPIC,
             groupId = "notification-group"
     )
     public void consume(OrderEvent orderEvent) {
-        System.out.println("Received Order Event: " + orderEvent.getEmail());
+        if (orderEvent.getStatus().equals("ORDER_PLACED")) {
+            System.out.println("Received Order Event: " + orderEvent.getEmail());
+            emailService.sendEmail(orderEvent.getEmail(), "Test", "WELCOME");
+        }
 
         String subject = "Order Status Update";
 
